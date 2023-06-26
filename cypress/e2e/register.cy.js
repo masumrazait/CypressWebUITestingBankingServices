46d3355451d07ac6391ae8b9022fb6b161ea7156
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-06-26 19:13:13 +0530

    code changes in register

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -3,22 +3,18 @@ describe('RegisterPage',()=>{
 
     it('register a user With valid details',()=>{
         cy.visit('https://parabank.parasoft.com/parabank/register.htm')
-
-       // cy.get("//*[text() = \"Register\"]").click()
-        cy.title().should('eq','ParaBank | Register for Free Online Account Access')
-        cy.get("//input[@id='customer.firstName']").type("Ravi")
-        cy.get("//input[@id='customer.lastName']").type("Sankar")
-        cy.get("//input[@id='customer.address.street']").type("Delhi")
-        cy.get("//input[@id='customer.address.city']").type("Dwarika")
-        cy.get("//input[@id='customer.address.state']").type("Delhi")
-        cy.get("//input[@id='customer.address.zipCode']").type("654334")
-        cy.get("//input[@id='customer.phoneNumber']").type("9898980077")
-        cy.get("//input[@id='customer.ssn']").type("56789")
-        cy.get("//input[@id='customer.username']").type("Ravi@123")
-        cy.get("//input[@id='customer.password']").type("Ravi@123")
-        cy.get("//input[@id='repeatedPassword']").type("Ravi@123")
-
-        cy.get("//input[@value='Register']").click()
+        cy.get('input#customer\\.firstName.input').type('John Doe');
+        cy.get('input#customer\\.lastName.input').type('Smith');
+        cy.get('input#customer\\.address\\.street.input').type('123 Main St');
+        cy.get('input#customer\\.address\\.city.input').type('Delhi');
+        cy.get('input#customer\\.address\\.state.input').type('Delhi');
+        cy.get('input#customer\\.address\\.zipCode.input').type('654334');
+        cy.get('input#customer\\.phoneNumber.input').type('9898980077');
+        cy.get('input#customer\\.ssn.input').type('56789');
+        cy.get('input#customer\\.username.input').type('Ravi@1234');
+        cy.get('input#customer\\.password.input').type('Ravi@1234');
+        cy.get('input#repeatedPassword.input').type('Ravi@1234');
+        cy.get('input[type="submit"].button[value="Register"]').click();
         cy.title().should('eq','ParaBank | Customer Created')
 
 
